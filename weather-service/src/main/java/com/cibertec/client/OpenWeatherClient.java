e89bf3b41ade0c2899c02e6561f93f65a28bf2fc
HaloGabriel <carlosgbm2000@gmail.com>
2026-04-07 22:59:47 -0500

    Crear OpenWeatherClient
    Crear OpenWeatherClient para consumo de API OpenWeather.

---
+++ weather-service/src/main/java/com/cibertec/client/OpenWeatherClient.java
@@ -0,0 +1,22 @@
+package com.cibertec.client;
+
+import com.cibertec.dto.external.OpenWeatherExternalDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "OPClient", url = "https://api.openweathermap.org")
+public interface OpenWeatherClient {
+
+    @GetMapping(
+            value = "/data/2.5/weather?units=metric",
+            produces = { "application/json" }
+    )
+    ResponseEntity<OpenWeatherExternalDTO> getWeatherData(
+            @RequestParam Double lat,
+            @RequestParam Double lon,
+            @RequestParam String appid,
+            @RequestParam(defaultValue = "es") String lang
+    );
+}
