96c9ecfa597f35c2c795da65bc77791aca48c8cf
Eddy1 <huamanieddy1@gmail.com>
2026-04-18 15:12:01 -0500

    Eliminar country-service completamente para poder subir otro corregido

--- country-service/src/main/java/country_service/exception/PaisNoEncontradoException.java
+++
@@ -1,8 +0,0 @@
-package country_service.exception;
-
-public class PaisNoEncontradoException extends RuntimeException {
-
-    public PaisNoEncontradoException(String mensaje) {
-        super(mensaje);
-    }
-}
