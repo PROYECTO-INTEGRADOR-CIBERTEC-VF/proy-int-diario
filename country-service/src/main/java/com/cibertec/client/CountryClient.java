353b00a76e86ccb770220ec97079edc120e848b2
Evelin Yupanqui Huisa <i202311813@cibertec.edu.pe>
2026-04-20 10:52:24 -0500

    Remove comments and clean up CountryClient methods

--- country-service/src/main/java/com/cibertec/client/CountryClient.java
+++ country-service/src/main/java/com/cibertec/client/CountryClient.java
@@ -13,25 +13,25 @@ public class CountryClient {
 
     private static final String BASE_URL = "https://restcountries.com/v3.1";
 
-    // 🔹 Buscar por código
+   
     public CountryExternal[] getByCode(String code) {
         String url = BASE_URL + "/alpha/" + code;
         return restTemplate.getForObject(url, CountryExternal[].class);
     }
 
-    // 🔹 Buscar por nombre
+
     public CountryExternal[] getByName(String name) {
         String url = BASE_URL + "/name/" + name;
         return restTemplate.getForObject(url, CountryExternal[].class);
     }
 
-    // 🔹 TODOS los países
+
     public CountryExternal[] getAllCountries() {
         String url = BASE_URL +  "/all?fields=name,cca2,region,capital,flags";
         return restTemplate.getForObject(url, CountryExternal[].class);
     }
 
-    // 🔹 Por región
+
     public CountryExternal[] getByRegion(String region) {
         String url = BASE_URL + "/region/" + region;
         return restTemplate.getForObject(url, CountryExternal[].class);
