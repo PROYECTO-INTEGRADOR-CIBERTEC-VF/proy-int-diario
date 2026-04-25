a4ac3100b4826fcb19cefa2024f85511c3b2fd2a
HaloGabriel <carlosgbm2000@gmail.com>
2026-04-25 08:38:26 -0500

    Modificar entidades JPA módulo Weather
    - Modificar entidad City.
    - Crear entidad Country.
    - Crear entidad State.

---
+++ weather-service/src/main/java/com/cibertec/model/Country.java
@@ -0,0 +1,23 @@
+package com.cibertec.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "countries")
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+}
