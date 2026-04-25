65615304d655ef5740db1293864c9010ef3ef6d6
Andy Toledo <andyjessietoledovilla@gmail.com>
2026-04-24 20:46:14 -0500

    nuevo endpoint para listar moneda

--- currency-service/src/main/java/com/cibertec/service/CurrencyService.java
+++ currency-service/src/main/java/com/cibertec/service/CurrencyService.java
@@ -1,13 +1,11 @@
 package com.cibertec.service;
 
-import org.springframework.stereotype.Service;
 import com.cibertec.dto.Currency;
-
 import java.math.BigDecimal;
 import java.util.Map;
 
-@Service
 public interface CurrencyService {
-    Map<String, Currency> getCurrenciesByCountryName(String name) ;
-    Object convert(String from, String to, BigDecimal amount);
+    Map<String, Currency> getCurrenciesByCountryName(String name);
+    Map<String, Object> convert(String from, String to, BigDecimal amount);
+    Map<String, String> getAvailableCurrencies();
 }
